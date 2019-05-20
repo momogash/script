86db8fc2138161a4afa85662ba5c3e94f805d441
Moloko MOGASHOA <momogash@c5r6s1.wethinkcode.co.za>
2019-05-20 16:51:58 +0200

    added clone command

--- voyager.sh
+++ voyager.sh
@@ -3,8 +3,12 @@
 bold=$(tput bold);
 blue=$(tput setaf 6);
 
+echo "----- Global -----"
 cat .bashrc >> ~/.bashrc;
 exec bash;
+
+echo "----- Github -----"
+git clone "https://github.com/momogash/Libft.git" ~/Desktop/libft
 
 echo "${blue}${bold}----- Finished -----${normal}";
 
